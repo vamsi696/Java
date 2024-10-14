6d9acf085f8dd395b26b8316fcd9292d996cc0a7
hemanthkandalla <vamsilionel@gmail.com>
2024-10-15 01:06:02 +0530

    SortedArray

---
+++ src/SortAnArray.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class SortAnArray {
+    public static void main(String[] args) {
+        int[] arr = {5, 2, 8, 1, 4};
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
