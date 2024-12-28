b67e15aa33ef6a606c6b36f07236bb14b7540f17
hemanthkandalla <vamsilionel@gmail.com>
2024-12-28 23:22:11 +0530

    Worked on the strings added the files and test cases

---
+++ src/String/String_operations.java
@@ -0,0 +1,23 @@
+//String Operations
+//Let us finish this module with some more practice!
+//
+//        Write a program which does the following:
+//
+//        Declare a string variable var.
+//        Assign the value "String" to it.
+//        And then print to the console the first 3 characters of the string.
+//        Do not include any space or new line.
+//        When you run the code, you will get a compilation error - go ahead and debug the code as well.
+
+package String;
+
+public class String_operations {
+    public static void main (String[] args)
+    {
+        // Update the blank in code given below
+        String var = "String";
+        System.out.print(var.charAt(0));  // Print the first 3 characters
+        System.out.print(var.charAt(1));
+        System.out.print(var.charAt(2));
+    }
+}
---
+++ src/String/Task_1.java
@@ -0,0 +1,16 @@
+//Write a program which does the following:
+//
+//Create a string variable word and assign the text "Programming" to it.
+//Print the characters o and r (only one) from the string word in separate lines using the syntax defined above.
+package String;
+
+public class Task_1 {
+    public static void main (String[] args)
+    {
+        String word = "Programming";
+        // Update your code below this line
+        System.out.println(word.charAt(2));
+        System.out.println(word.charAt(4));
+
+    }
+}
